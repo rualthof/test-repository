9416ee9c7d73cd52995d7401ea2460bec8e3b26e
rualthof <rualthof@hotmail.com>
2020-04-16 20:41:18 +0000

    refresh token implemented

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_restful import Resource, Api
 from flask_jwt_extended import JWTManager
 # from flask_jwt import JWT
 # from security import authenticate, identity
-from resources.user import UserRegister, User, UserLogin
+from resources.user import UserRegister, User, UserLogin, TokenRefresh
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
@@ -16,8 +16,7 @@ app.secret_key = "rudimar"  # if publishing, do not want this key to be visible
 api = Api(app)
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # the sqlAlchemy modification tracker does the job
-app.config[
-    'SQLALCHEMY_DATABASE_URI'] = 'postgres://ugykgauycmywxv:701bf83f2c3578d738436980432836bcfed076dbedbe70f29aacef014dbfab16@ec2-18-233-137-77.compute-1.amazonaws.com:5432/derqg7tdiv2err'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ugykgauycmywxv:701bf83f2c3578d738436980432836bcfed076dbedbe70f29aacef014dbfab16@ec2-18-233-137-77.compute-1.amazonaws.com:5432/derqg7tdiv2err'
 # second is the default value
 # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['PROPAGATE_EXCEPTION'] = True
@@ -53,6 +52,7 @@ api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
 api.add_resource(User, '/user/<int:user_id>')
 api.add_resource(UserLogin, '/login')
+api.add_resource(TokenRefresh, '/refresh')
 
 if __name__ == "__main__":
     print("oi")
---
+++ old_security.py
@@ -0,0 +1,16 @@
+from werkzeug.security import safe_str_cmp
+from models.user import UserModel
+
+
+def authenticate(username, password):
+    print(username, password)
+    user = UserModel.find_by_username(username)
+    print(user)
+    if user and safe_str_cmp(user.password, password):
+        return user
+
+
+def identity(payload):
+    user_id = payload['identity']
+    print(user_id)
+    return UserModel.find_by_id(user_id)
